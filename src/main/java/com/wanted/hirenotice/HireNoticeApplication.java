7e21059bb2383266e9de6c8f0948615158dc2ea9
hamwoojo <woojo3398@naver.com>
2023-10-19 17:35:59 +0900

    [feat] project init

---
+++ src/main/java/com/wanted/hirenotice/HireNoticeApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.hirenotice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireNoticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireNoticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wanted/hirenotice/HireNoticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.hirenotice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireNoticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
